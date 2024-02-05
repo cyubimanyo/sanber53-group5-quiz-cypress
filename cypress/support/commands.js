50d7106ebeaf68f49042429bc34ffc97647d6696
cyubimanyo <andhiny.aye@gmail.com>
2024-02-05 16:12:16 +0700

    push signin features with 5 test cases

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,11 @@ Cypress.Commands.add('signup', (firstName, lastName, emailAddress, password, pas
     cy.get('#password-confirmation').should('be.visible').type(passwordConfirmation)
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').should('be.visible').click()
 })
+
+Cypress.Commands.add('login', (emailAddress, password) => {
+    cy.get('a').contains('Sign In').click()
+    cy.scrollTo('bottom')
+    cy.get('#email').should('be.visible').type(emailAddress)
+    cy.get('#pass').should('be.visible').type(password)
+    cy.get('#send2').should('be.visible').click()
+})
